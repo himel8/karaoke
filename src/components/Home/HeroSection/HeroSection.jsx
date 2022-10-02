97f7a106b8f927d4c357981546820015fc636a36
himel8 <himelkhan290@gmail.com>
2022-10-02 10:28:12 +0600

    environment setup

---
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <div className="">
+        <h6 className="text-xl font-bold text-center">
+          WELCOME TO KARAOKE LOUNGE
+        </h6>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "'Raleway', sans-serif",
+        text: "'Manrope', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
